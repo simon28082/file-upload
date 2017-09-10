5127a0ee23fd73655749ffa3d8b0d324bb9ce639
crcms <crcms@crcms.cn>
2017-09-10 20:43:31 +0800

    clear and format

--- src/Contracts/FileUpload.php
+++ src/Contracts/FileUpload.php
@@ -6,11 +6,11 @@ namespace CrCms\Upload\Contracts;
  * Interface FileUpload
  *
  * @package CrCms\Upload\Contracts
- * @author simon
  */
 interface FileUpload
 {
     /**
+     * @param array $file
      * @return array
      */
     public function upload(array $file): array;
